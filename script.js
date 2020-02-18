2ec616b01b87dda9870ed4bf5b2f1ba7c1078a94
Edd Sansome <edd@murismo.com>
2020-02-18 10:20:46 +0000

    add delete button functionality

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ let myLibrary = []
 let myForm = []
 let addBookToScreen = document.getElementById('addButton');
 let button = addBookToScreen.addEventListener('click', () => bookForm());
+let deleteBook = document.getElementsByClassName('deleteButton')
+console.log(deleteBook)
+
+
+// deleteButton.addEventListener('click', () => testFunc(obj));
+
+
 
 window.onload = () => addGrid();
+
 
 
 
 function Book(title, author, pages, yearPublished, readStatus) {
 //book object constructor
 
-this.title = title,
-this.author = author,
-this.pages = pages,
-this.yearPublished = yearPublished,
-this.readStatus = readStatus
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.yearPublished = yearPublished,
+    this.readStatus = readStatus
 }
 
 function addBookToLibrary(title, author, pages, yearPublished, readStatus) {
@@ -32,10 +40,23 @@ function addBookToLibrary(title, author, pages, yearPublished, readStatus) {
 
 }
 
+function testFunc(x) {
 
-// addBookToLibrary('The Hobbit', 'J.R.R Tolkein', 287, 1955, true)
-// addBookToLibrary('The Lord of the Rings', 'J.R.R Tolkein', 526, 1958, false)
-// addBookToLibrary('Game of Thrones', 'George RR Martin', 627, 1998, false)
+    
+    let id = x.replace('bookButton-','')
+    console.log(`delete button ${id} is working`)
+    console.log(typeof(id))
+
+    
+    myLibrary.splice(id, 1)
+    refreshGrid();
+    addGrid();
+    clearInputs();
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R Tolkein', 287, 1955, true)
+addBookToLibrary('The Lord of the Rings', 'J.R.R Tolkein', 526, 1958, false)
+addBookToLibrary('Game of Thrones', 'George RR Martin', 627, 1998, false)
 
 function addGrid() {
 
@@ -43,6 +64,8 @@ function addGrid() {
 
     for(obj in myLibrary) {
         gridElement = document.createElement('div');
+        
+
         gridElement.innerHTML = `Title: ${myLibrary[obj].title}<br>
                                  Author: ${myLibrary[obj].author}<br>
                                  Pages: ${myLibrary[obj].pages}<br>
@@ -52,13 +75,50 @@ function addGrid() {
 
         gridElement.id = `bookObject${obj}`
         gridElement.classList.add('bookObj');
+
+       
+
         let node = document.getElementById('bookshelf');
+        
+
         node.appendChild(gridElement)
+
+        buttonElement = document.createElement('button')
+        buttonElement.id = `bookButton-${obj}`
+        buttonElement.classList.add('deleteButton');
+        let gridNode = document.getElementById(gridElement.id)
+        gridNode.appendChild(buttonElement)
+        
+        
+        
+
+      
+
+        // buttonElement = document.createElement('button')
+        // buttonElement.id = `readButton${obj}`
+        // buttonElement.classList.add('readButton');
+        // let gridNode = document.getElementById(gridElement.id)
+        // gridNode.appendChild(buttonElement)
+        
 
     }
 
+    let deleteButton = document.querySelectorAll('.deleteButton')
+    
+
+   for(let button of deleteButton) {
+       button.addEventListener('click', () => testFunc(button.id))
+   }
+    
+
+ 
+ 
+
+    
 
 }
+
+
 
 function refreshGrid() {
 
